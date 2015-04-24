37be6e722ac07aca63e88fbabc40fd886dc009a4
UltraPeeks <klahammer@web.de>
2015-04-24 02:19:26 +0200

    Removed unused code
    - Removed SolderingIron.java
    - Version increased to 1.4a

--- src/main/java/de/ultrapeeks/gemblocksforgreg/GemBlocksForGregMod.java
+++ src/main/java/de/ultrapeeks/gemblocksforgreg/GemBlocksForGregMod.java
@@ -24,7 +24,7 @@ import de.ultrapeeks.gemblocksforgreg.BasicGemBlock.GemType;
 public class GemBlocksForGregMod {
     public static final String MODID = "GemBlocksForGreg";
     public static final String NAME = "GemBlocksForGreg";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.4a";
     
     @Instance(value = MODID)
     public static GemBlocksForGregMod instance;
